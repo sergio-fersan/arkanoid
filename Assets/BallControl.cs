d8b4eecafc4006faaeb7eafc255c61bffb3c4e33
sergio-fersan <sergio.fersan.sss@gmail.com>
2026-09-04 00:22:29 -0300

    nave e bola funcionando basico

---
+++ Assets/BallControl.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();     // Inicializa o objeto bola
+        Invoke("GoBall", 2);                    // Chama a função GoBall após 2 segundos
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // inicializa a bola randomicamente para esquerda ou direita, para cima
+    void GoBall(){                      
+        float rand = Random.Range(0, 2);
+        if(rand < 1){
+            rb2d.AddForce(new Vector2(20, 15));
+        } else {
+            rb2d.AddForce(new Vector2(-20, 15));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll){
+        if (coll.collider.CompareTag("Player"))
+        {
+            Vector2 vel = rb2d.linearVelocity;
+
+            // Dá uma influência da velocidade da nave
+            vel.y += coll.collider.attachedRigidbody.linearVelocity.y * 0.3f;
+
+            // Mantém uma velocidade mínima
+            float velocidade = 6f;
+            vel = vel.normalized * velocidade;
+
+            rb2d.linearVelocity = vel;
+        }
+    }
+
+    // Reinicializa a posição e velocidade da bola
+    void ResetBall(){
+        rb2d.linearVelocity = Vector2.zero;
+        transform.position = Vector2.zero;
+    }
+
+    // Reinicializa o jogo
+    void RestartGame(){
+        ResetBall();
+        Invoke("GoBall", 1);
+    }
+
+}
---
+++ Assets/NaveControl.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class NaveControl : MonoBehaviour
+{
+    public KeyCode moveLeft = KeyCode.A;      // Move a nave para a esquerda
+    public KeyCode moveRight = KeyCode.D;    // Move a nave para a direita
+    public float speed = 6.0f;             // Define a velocidade da nave
+    public float boundX = 2.5f;            // Define os limites em X
+    private Rigidbody2D rb2d;               // Define o corpo rigido 2D que representa a nave
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();     // Inicializa a nave
+    }
+
+    void Update()
+    {
+        var vel = rb2d.linearVelocity;                // Acessa a velocidade da nave
+        if (Input.GetKey(moveRight)) {             // Velocidade da nave para ir para a esquerda
+            vel.x = speed;
+        }
+        else if (Input.GetKey(moveLeft)) {      // Velocidade da nave para ir para a direita
+            vel.x = -speed;                    
+        }
+        else {
+            vel.x = 0;                          // Velociade para manter a nave parada
+        }
+        rb2d.linearVelocity = vel;                    // Atualizada a velocidade da nave
+
+        var pos = transform.position;           // Acessa a Posição da nave
+        if (pos.x > boundX) {                  
+            pos.x = boundX;                     // Corrige a posicao da nave caso ele ultrapasse o limite direito
+        }
+        else if (pos.x < -boundX) {
+            pos.x = -boundX;                    // Corrige a posicao da nave caso ele ultrapasse o limite esquerdo
+        }
+        transform.position = pos;               // Atualiza a posição da nave
+
+    }
+}
